d237a294791564ec4757f3c23909a09512894a8d
West <agus2kcoin@outlook.com>
2023-04-11 20:18:05 -0300

    Add progress/newPosition

--- shaders/vertex.glsl.js
+++ shaders/vertex.glsl.js
@@ -4,18 +4,30 @@ const vertexShader = /* glsl */ `
   varying float vRotation;
   varying float vAlpha;
 
+  uniform float uTime;
+  uniform float uProgress;
+
   attribute vec3 aColors;
   attribute float aRotation;
   attribute float aSize;
   attribute float aAlpha;
+  attribute vec3 aPosition;
 
   void main() {
     vUv = uv;
     vColors = aColors;
     vRotation = aRotation;
-    vAlpha = aAlpha;
+    vAlpha = aAlpha * (1. - uProgress);
 
-    vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
+    vec3 newPosition = position;
+
+    float dist = distance(newPosition, vec3(-1., 0., 0.));
+
+    newPosition = mix(newPosition, aPosition, uProgress);
+    newPosition.x += sin(dist * 5. + uTime) * 0.002;
+    newPosition.y += sin(dist * 5. + uTime) * 0.008;
+
+    vec4 mvPosition = modelViewMatrix * vec4( newPosition, 1. );
     gl_PointSize = aSize * 40. * ( 1. / - mvPosition.z );
     gl_Position = projectionMatrix * mvPosition;
   }
